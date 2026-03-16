fb5e99b69ef5dd3233d25ce86e082c90f8a568b4
Lazysusan01 <nicomcgill@gmail.com>
2026-03-16 20:44:35 +0000

    fix: suppress exhaustive-deps lint error in useReveal to unblock CI build

--- src/hooks/useReveal.ts
+++ src/hooks/useReveal.ts
@@ -18,6 +18,7 @@ export function useReveal(options?: IntersectionObserverInit) {
     )
     observer.observe(el)
     return () => observer.disconnect()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return ref
